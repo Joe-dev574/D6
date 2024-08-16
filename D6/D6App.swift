9dea1d56486b425411b97042d4751da5bcde001b
JDeWeese <jdeweese574@icloud.com>
2024-08-16 14:00:29 -0500

    Initial Commit

---
+++ D6/D6App.swift
@@ -0,0 +1,17 @@
+//
+//  D6App.swift
+//  D6
+//
+//  Created by Joseph DeWeese on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct D6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
